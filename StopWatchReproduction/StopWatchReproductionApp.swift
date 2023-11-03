e225cb588b34f4862b1be07d2c4e0a90a2a1afea
AJ Cooper <acooper@lcs.on.ca>
2023-11-03 09:27:52 -0400

    Initial Commit

---
+++ StopWatchReproduction/StopWatchReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchReproductionApp.swift
+//  StopWatchReproduction
+//
+//  Created by  Adam-James  Cooper on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
